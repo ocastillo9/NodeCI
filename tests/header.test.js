2152bdee5682c042104bce7ecc6c80f9d3834b37
Oscar Castillo <oscarcastillov@gmail.com>
2020-11-17 14:44:53 -0300

    Bypassing oauth for testing using session id

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,7 +12,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-    await browser.close()
+    //await browser.close()
 });
 
 test('the header has the correct text', async () => {
@@ -21,9 +21,38 @@ test('the header has the correct text', async () => {
 });
 
 test('clicking login  starts oatuh flow', async () => {
-    const text = await page.click('.right a');
+    await page.click('.right a');
 
-    const url = await page.url()
+    const url = await page.url();
 
     expect(url).toMatch('accounts\.google\.com')
+});
+
+test.only('when signed in, show logout button', async () => {
+    const id = '5fb2a5948d16238dc3f57b0c';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    };
+
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
+
+    const Keygrip = require('keygrip')
+    const keys = require('../config/keys')
+    const keygrip = new Keygrip([keys.cookieKey])
+    const sig = keygrip.sign('session='+ sessionString)
+
+    await page.setCookie({name: 'session', value: sessionString})
+    await page.setCookie({name: 'session.sig', value: sig})
+
+    await page.goto('localhost:3000')
+
+    await page.waitFor('a[href="/auth/logout"]')
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(text).toMatch('Logout')
 });
