765102db725442224f752273804108067e61770a
Oscar Castillo <oscarcastillov@gmail.com>
2020-11-16 22:00:29 -0300

    clearing nested hashes

--- services/cache.js
+++ services/cache.js
@@ -1,35 +1,37 @@
-const mongoose =  require('mongoose')
-const redis =  require('redis')
-const util =  require('util')
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
 
-const redisURL =  'redis://localhost:6379'
-const client =  redis.createClient(redisURL)
-client.get = util.promisify(client.get)
+const redisURL = 'redis://localhost:6379';
+const client = redis.createClient(redisURL);
+client.hget = util.promisify(client.hget);
 
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function (){
-    this.useCache = true
+mongoose.Query.prototype.cache = function (options = {}) {
+    this.useCache = true;
+    this.hasKey = JSON.stringify(options.key || '');
+
     return this
-}
+};
 
 mongoose.Query.prototype.exec = async function () {
-    
+
     if (!this.useCache) {
         return exec.apply(this, arguments)
     }
 
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
-    }))
+    }));
     // console.log(key)
 
-    const cacheValue = await client.get(key)
+    const cacheValue = await client.hget(this.hasKey, key);
 
     if (cacheValue) {
-        const doc = JSON.parse(cacheValue)
+        const doc = JSON.parse(cacheValue);
 
         return Array.isArray(doc)
             ? doc.map(d => new this.model(d))
@@ -37,10 +39,15 @@ mongoose.Query.prototype.exec = async function () {
     }
 
 
-
-    const result = await exec.apply(this, arguments)
-    client.set(key, JSON.stringify(result))
+    const result = await exec.apply(this, arguments);
+    client.hset(this.hasKey, key, JSON.stringify(result), 'EX', 120);
 
 
     return result;
-}
+};
+
+module.exports = {
+    clearHash(hasKey) {
+        client.del(JSON.stringify(hasKey));
+    }
+};
