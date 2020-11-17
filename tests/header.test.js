a7b35792a7ffed7cdaab3d4241658b1e41a9430a
Oscar Castillo <oscarcastillov@gmail.com>
2020-11-17 12:12:47 -0300

    Integration test with puppeteer

---
+++ tests/header.test.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+
+    page = await browser.newPage();
+    await page.goto('localhost:3000')
+});
+
+afterEach(async () => {
+    await browser.close()
+});
+
+test('the header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster')
+});
+
+test('clicking login  starts oatuh flow', async () => {
+    const text = await page.click('.right a');
+
+    const url = await page.url()
+
+    expect(url).toMatch('accounts\.google\.com')
+});
